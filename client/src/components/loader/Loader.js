c8847b5e898103e260b130d8331336bdc3b169eb
Nathaniel Hernandez <nethanielh@me.com>
2022-04-07 14:22:32 -0600

    changed loader

--- client/src/components/loader/Loader.js
+++ client/src/components/loader/Loader.js
@@ -4,7 +4,7 @@ import "./Loader.css";
 const Loader = () => {
   return (
     <div className="lds-wrapper">
-      <div class="lds-ellipsis">
+      <div className="lds-ellipsis">
         <div></div>
         <div></div>
         <div></div>
